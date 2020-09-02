4b253f18da21ddb7669dae6efd41bc4d14cb7b09
Will <williamfrederickkho@gmail.com>
2020-09-02 23:38:48 +1200

    boop

--- BookReview.java
+++ BookReview.java
@@ -1,3 +1,4 @@
+ 
 /* Based on the VUW ecs100 template
  *
  */
@@ -19,10 +20,13 @@ import java.util.HashMap;
 public class BookReview{
     
     public String bookAuthor,bookName,bookGenre; // variables for book name
-    ArrayList<Book> books = new ArrayList<Book>();
+    ArrayList<Book> bookArray = new ArrayList<Book>();
+    HashMap<String, String> bookRating = new HashMap<String,String>();
     HashMap<String, Book> bookMap = new HashMap<String,Book>();
     private int bookTotal = 0;
     
+    Button like = new Button(true);
+    Button dislike = new Button(false);
     
     
     /**
@@ -43,11 +47,11 @@ public class BookReview{
         
         //BookReview br = new BookReview();
         
-        UI.setMouseListener(this::manageMouse);
+        
         mainMenu();
         UI.setDivider(1);
-        Button like = new Button(true);
-        Button dislike = new Button(false);
+        UI.clearGraphics();
+        
         
         // UI.addTextField("Book Name", br::setBookName);
         // UI.addTextField("Book Author", br::setBookAuthor);
@@ -55,11 +59,16 @@ public class BookReview{
         // UI.setKeyListener(br::test);
     }
     public void mainMenu(){
+        UI.setDivider(1);
         UI.addButton("Quit", UI::quit); 
         UI.addButton("Add Book", this::addBook);
-        UI.addButton("Search Book", UI::quit);
-        UI.addButton("Rate Book", UI::quit);
-        UI.addButton("Recommend Book", UI::quit);
+        if(bookTotal <= 0 != true) {
+            UI.addButton("Delete Book", this::deleteBook);
+            UI.addButton("Search Book", this::searchBook);
+            UI.addButton("Rate Book", this::rateBook);
+            UI.addButton("Recommend Book", this::recommendBook);
+        }
+        
     }
    
     
@@ -68,24 +77,98 @@ public class BookReview{
      * 
      */
     
-    public void addBook(){
-        UI.initialize;
+    public void addBook() { 
+        UI.initialise(); 
+        UI.setDivider(1);
         setBookName(UI.askString("Enter the title of the book: "));
         setBookAuthor(UI.askString("Enter the author of the book: "));
         setBookGenre(UI.askString("Enter the genre of the book: "));
         //books.add(new Book(bookName, bookAuthor, bookGenre))
-        Book book = new Book(bookName, bookAuthor, bookGenre, bookTotal);
+        Book book = new Book(bookName, bookAuthor, bookGenre);
         bookMap.put(bookName, book);
+        UI.clearText();
         UI.println(bookName + " has been sucessfully added!");
         bookTotal += 1;
-      
         
-        
-        
-        
-        
+        mainMenu();
+
+    }
+    public void rateBook() {
+        UI.initialise(); 
+        boolean check = false;
+        while (check == false) {
+            String searchString = UI.askString("Enter the title of the book to rate: ");
+            if(checkBook(searchString) == true) {
+                
+                check = true;
+            }
+            else{
+                UI.println("That book isn't in the database.");
+            }
+        }
+        UI.setMouseListener(this::mouseRate);
+        boolean rated = false;
+        UI.setDivider(0);
+        while (rated != true) {
+            rated = true;
+        }
+        mainMenu();
     }
     
+    public void deleteBook() {
+        UI.initialise(); 
+        
+        boolean check = false;
+        while (check == false) {
+        String searchString = UI.askString("Enter the title of the book to delete: ");
+        if(checkBook(searchString) == true) {
+            check = true;
+            bookMap.remove(searchString);
+            bookTotal -= 1;
+        }
+        else{
+            UI.println("That book isn't in the database.");
+        }
+        
+        }
+        mainMenu();
+    }
+    
+    public void recommendBook() {
+        UI.initialise(); 
+        
+        mainMenu();
+    }
+    
+    public void searchBook() {
+        UI.initialise(); 
+        boolean check = false;
+        while (check == false) {
+            
+            String searchString = UI.askString("Enter the title of the book to search: ");
+            if(checkBook(UI.askString(searchString)) == true) {
+                check = true;
+                
+            }
+            else{
+                UI.println("That book isn't in the database.");
+            }
+            mainMenu();
+        }
+    }
+    /**
+     *  Checks to see if book exists
+     *  @string searchTerm - searchTerm to be checked
+     */
+    public boolean checkBook(String searchTerm){
+        if(bookMap.containsKey(searchTerm)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+            
     /**
      * 
      */
@@ -113,18 +196,14 @@ public class BookReview{
     /**
      * 
      */
-    public void manageMouse (String action, double x, double y) {
+    public boolean mouseRate (String action, double x, double y) {
         if(action.equals("clicked")) {
-            
-            // for (Map.Entry<String, Book> entry : bookMap.entrySet())
-            // {
-               // String key = entry.getKey();
-               // Book value = entry.getValue();
-               // if (value.onButton(x,y) == true) {
-                   // value.placeholder();
-                // }
-               // //UI.println(value.placeholder());
-            // }
+            if(like.onButton(x,y) == true) {
+                like.placeholder();
+            }
+            if(dislike.onButton(x,y) == true ) {
+                dislike.placeholder();
+            }
         }
     }
 }
